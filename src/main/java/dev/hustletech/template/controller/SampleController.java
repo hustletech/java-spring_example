6978d243211d86b52647bd7c158f26543aa293c5
Rodrigo Dantas <rodrigo.dantas-ext@viavarejo.con.br>
2020-12-27 00:13:29 -0300

    swagger 3

---
+++ src/main/java/dev/hustletech/template/controller/SampleController.java
@@ -0,0 +1,16 @@
+package dev.hustletech.template.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample")
+public class SampleController {
+
+    @GetMapping("/")
+    public String get() {
+        return "worked";
+    }
+    
+}
